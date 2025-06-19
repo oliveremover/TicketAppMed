a458107a9889b1b45216a3a2ef296c33deab5498
Carl <carlwskills@gmail.com>
2025-06-19 19:29:04 +0200

    fix: almost finished step 3 TODO: - Confirmation button

--- BlazorApp1/Models/WizardModel.cs
+++ BlazorApp1/Models/WizardModel.cs
@@ -3,9 +3,10 @@ namespace BlazorApp1.Models
 {
     public class WizardModel
     {
-        public string Choice1 { get; set; }
-        public string Choice2 { get; set; }
-        public string Choice3 { get; set; }
-        public string Choice4 { get; set; }
+        public string Category { get; set; }
+        public string Equipment { get; set; }
+        public string Header { get; set; }
+        public string Details { get; set; }
+        public List<string> Files { get; set; } = new List<string>();
     }
 }
