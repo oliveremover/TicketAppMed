62a897874427bd6cae2507c3d069cfaff040692c
Carl <carlwskills@gmail.com>
2025-06-19 16:15:57 +0200

    feat: Added first step of wizard.
    TODO:
    - Can't get the steps to work.
    - Can't get the navigationManager to work as intended.

---
+++ BlazorApp1/Models/WizardModel.cs
@@ -0,0 +1,11 @@
+using System;
+namespace BlazorApp1.Models
+{
+    public class WizardModel
+    {
+        public string Choice1 { get; set; }
+        public string Choice2 { get; set; }
+        public string Choice3 { get; set; }
+        public string Choice4 { get; set; }
+    }
+}
