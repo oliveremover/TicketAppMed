78e84cbf03b0b045d0fe1de335e66f7446072934
bylund.c <bylundcarl39@gmail.com>
2025-06-23 14:14:08 +0200

    fix: added nonnullable on wizardmodel

--- BlazorApp1/Models/WizardModel.cs
+++ BlazorApp1/Models/WizardModel.cs
@@ -2,10 +2,10 @@ namespace BlazorApp1.Models
 {
     public class WizardModel
     {
-        public string Category { get; set; }
-        public string Equipment { get; set; }
-        public string Header { get; set; }
-        public string Details { get; set; }
+        public string? Category { get; set; }
+        public string? Equipment { get; set; }
+        public string? Header { get; set; }
+        public string? Details { get; set; }
         public List<string> Files { get; set; } = new List<string>();
     }
 }
